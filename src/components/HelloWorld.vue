df8041466dad2d66edff4bb27f1d61994bc0be9c
Robin Tijou <robintijou05@gmail.com>
2025-01-30 14:10:42 +0100

    first commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,63 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import { useMouse, usePreferredDark } from '@vueuse/core'
+
+
+
+
+defineProps<{ msg: string }>()
+
+// Déclaration d'une variable « observé » de type Int
+const count = ref(0)
+
+// Déclaration d'une variable « observé » de type String
+const input = ref('Votre nom')
+  // tracks mouse position
+  const { x, y } = useMouse()
+
+  // is user prefers dark theme
+  const isDark = usePreferredDark()
+
+  const modal = ref(false)
+
+const ignoreElRef = ref()
+
+
+
+</script>
+
+<template>
+  <div v-if="isDark">Dark mode</div>
+  <div v-else>Light mode</div>
+  <h1>{{ msg }}</h1>
+
+  <div class="card">
+    <button type="button" @click="count--">--</button>
+    <button type="button">{{ count }}</button>
+    <button type="button" @click="count++">++</button>
+  </div>
+
+  <div class="card">
+    <h2>hello « {{ input }} » {{ x }}, {{ y }}</h2>
+    <input v-model="input" />
+  </div>
+
+  <button @click="modal = true">
+    Open Modal
+  </button>
+
+  <div ref="ignoreElRef">
+    click outside ignore element
+  </div>
+
+
+</template>
+
+<style scoped>
+.card {
+  background: #595959;
+  border: 1px solid #ccc;
+  border-radius: 2px;
+  margin: 10px;
+}
+</style>
